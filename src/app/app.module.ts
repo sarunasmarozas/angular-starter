0c805f9400a8fb2b51d671ef501d0e34cc2c03fa
Sarunas M <sarunas.marozas@metasite.net>
2021-02-11 16:27:24 +0200

    install ng-bootstrap, add modalModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { ModalModule } from "ngb-modal";
 import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
@@ -8,6 +7,7 @@ import { AppComponent } from "./app.component";
 import { ComponentAComponent } from "./components/component-a/component-a.component";
 import { ComponentBComponent } from "./components/component-b/component-b.component";
 import { ComponentCComponent } from "./components/component-c/component-c.component";
+import { NgbModalModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
@@ -16,7 +16,7 @@ import { ComponentCComponent } from "./components/component-c/component-c.compon
     ComponentBComponent,
     ComponentCComponent,
   ],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, ModalModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, NgbModalModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/component-b/component-b.component.ts
+++ src/app/components/component-b/component-b.component.ts
@@ -1,14 +1,18 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, ElementRef, Input, ViewChild } from "@angular/core";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: "app-component-b",
   templateUrl: "./component-b.component.html",
   styleUrls: ["./component-b.component.css"],
 })
-export class ComponentBComponent implements OnInit {
+export class ComponentBComponent {
   @Input() title: string;
+  @ViewChild("modal", { static: true }) modal: ElementRef;
 
-  constructor() {}
+  constructor(private modalService: NgbModal) {}
 
-  ngOnInit() {}
+  openModal() {
+    this.modalService.open(this.modal, { centered: true, size: "md" });
+  }
 }
